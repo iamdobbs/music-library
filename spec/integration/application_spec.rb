2402dd9189b9da7a4088f686d6440687a5a645fa
Hayley Dobbs <iamdobbs@icloud.com>
2023-02-07 15:35:29 +0000

    Initial Project Setup from Makers Clone

---
+++ spec/integration/application_spec.rb
@@ -0,0 +1,41 @@
+require "spec_helper"
+require "rack/test"
+require_relative '../../app'
+
+describe Application do
+  # This is so we can use rack-test helper methods.
+  include Rack::Test::Methods
+
+  # We need to declare the `app` value by instantiating the Application
+  # class so our tests work.
+  let(:app) { Application.new }
+
+  context 'GET /albums'  do
+    it 'should return the list of albums' do
+      response = get('/albums')
+
+      expected_response = "Surfer Rosa, Waterloo, Super Trouper, Bossanova, Lover, Folklore, I Put a Spell on You, Baltimore, Here Comes the Sun, Fodder on My Wings, Ring Ring"
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq(expected_response)
+    end
+  end    
+
+  context 'POST /albums' do
+    it 'should create a new album' do
+      response = post(
+        '/albums',
+        title: 'All We Know',
+        release_year: '2016',
+        artist_id: '2'
+      )
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq('')
+
+      response = get('/albums')
+
+      expect(response.body).to include('All We Know')
+    end
+  end    
+end
